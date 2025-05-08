edecaa7e481711e11d0161282ea0f3789fbada11
Ansyar <m.ansyarafi@gmail.com>
2025-05-08 19:14:49 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+let correctAnswer = '';
+let currentQuestion = '';
+let currentAnswer = [];
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/get-question', async (req, res) => {
+    try {
+        const response = await axios.get('https://opentdb.com/api.php?amount=1&type=multiple');
+        const question = response.data.results[0];
+        currentQuestion = question.question;
+        correctAnswer = question.correct_answer;
+        console.log(correctAnswer);
+        question.incorrect_answers.push(correctAnswer);
+        question.incorrect_answers.sort(() => Math.random() - 0.5); // Shuffle answers
+        currentAnswer = question.incorrect_answers;
+        // console.log(question);
+        res.render('index', { question: question.question, answer: question.incorrect_answers});
+    } catch (error) {
+        console.error('Error fetching question:', error);
+        res.render('index', {error: 'Try again in a few seconds!'});
+    }
+}
+);
+
+app.post('/submit-answer', (req, res) => {
+    const userAnswer = req.body.answer;
+    console.log(userAnswer);
+    console.log(correctAnswer);
+  
+    let result;
+    if (userAnswer.trim() === correctAnswer.trim()) {
+      result = {
+        title: 'Correct!',
+        message: 'Nice job 🎉',
+        icon: 'success'
+      };
+    } else {
+      result = {
+        title: 'Incorrect!',
+        message: 'Try again ❌',
+        icon: 'error'
+      };
+    }
+  
+    res.render('index', { question: currentQuestion, answer: currentAnswer, result: result });
+  });
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
